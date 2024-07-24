01b14953b7ce49dec5f1064d978ad773c02720a9
Furkan Taş <73059531+furrkanntas@users.noreply.github.com>
2024-07-24 19:12:22 +0300

    v1.1 | Added class structure.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,34 +1,61 @@
-const correctAnswers = ['B', 'A', 'B', 'B'];
-const form = document.querySelector('.quiz-form');
-const result = document.querySelector('.result');
+import { Question } from './question.js';
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
+const quizRoom = document.querySelector('.quiz-rooms');
+const quizQuestions = document.querySelector('.quiz');
+const textTopicDiv = document.querySelector('.textTopic');
 
-  let score = 0;
-  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+let quizData = {}; 
 
-  // check answers
-  userAnswers.forEach((answer, index) => {
-    if (answer === correctAnswers[index]) {
-      score += 25;
+async function fetchQuizData() {
+    try {
+        const response = await fetch('/public/questions.json');
+        quizData = await response.json();
+    } catch (error) {
+        console.error('Error fetching the JSON file:', error);
     }
-  });
+}
 
-  // show result on page
-  scrollTo(0,0);
-
-  result.classList.remove('d-none');
-
-  let output = 0;
-  const timer = setInterval(() => {
-    result.querySelector('span').textContent = `${output}%`;
-    if(output === score) {
-      clearInterval(timer);
-    } else {
-      output++;
+async function setDefaultTopic() {
+    await fetchQuizData(); 
+    const defaultButton = document.querySelector('#HTML');
+    if (defaultButton) {
+        defaultButton.click(); 
     }
-  }, 10);
+}
 
+quizRoom.addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') {
+        const buttonText = e.target.textContent;
+        textTopicDiv.textContent = buttonText;
+
+        textTopicDiv.classList.remove('border-green-300', 'border-blue-300', 'border-orange-300', 'border-purple-300');
+
+        const buttonId = e.target.getAttribute('id');
+        if (buttonId === 'HTML') {
+            textTopicDiv.classList.add('border-green-300');
+        } else if (buttonId === 'CSS') {
+            textTopicDiv.classList.add('border-blue-300');
+        } else if (buttonId === 'JavaScript') {
+            textTopicDiv.classList.add('border-orange-300');
+        } else if (buttonId === 'GIT') {
+            textTopicDiv.classList.add('border-purple-300');
+        }
+
+        if (quizData[buttonId]) {
+            loadQuestions(buttonId);
+        } else {
+            console.error('No data available for the selected topic');
+        }
+    }
 });
 
+async function loadQuestions(buttonId) {
+    const questions = quizData[buttonId];
+    quizQuestions.innerHTML = ''; // Önceki soruları temizle
+    questions.forEach((questionData, index) => {
+        const question = new Question(index, questionData);
+        quizQuestions.appendChild(question.createQuestionElement());
+    });
+}
+
+window.addEventListener('load', setDefaultTopic);
---
+++ public/scripts/question.js
@@ -0,0 +1,42 @@
+export class Question {
+  constructor(index, questionData) {
+      this.index = index;
+      this.questionText = questionData.question;
+      this.options = questionData.options;
+  }
+
+  createQuestionElement() {
+      const questionDiv = document.createElement('div');
+      questionDiv.classList.add('list-group-item', 'flex', 'items-start', 'gap-2', 'bg-white/30', 'mt-3', 'rounded-xl', 'border-l-4', 'border-orange-300', 'hover:border-blue-300', 'shadow-md');
+      questionDiv.innerHTML = `
+      <div class="flex flex-col p-3 pr-6">
+        <div class="flex items-center gap-2">
+          <i class="fa-solid fa-door-closed text-orange-600 text-2xl"></i>
+          <i class="fa-solid fa-door-open text-orange-600 text-2xl hidden"></i>
+          <span class="font-bold username">q-${this.index + 1}:</span>
+          <span class="message font-semibold">${this.questionText}</span>
+          <i class="fa-solid fa-question text-orange-600 text-xl"></i>
+        </div>
+        <div class="flex flex-col items-start justify-center gap-2 mt-4 mb-3">
+          ${this.createOptionsElements()}
+        </div>
+      </div>
+      `;
+      return questionDiv;
+  }
+
+  createOptionsElements() {
+      return Object.entries(this.options)
+          .map(([key, value]) => this.createOptionElement(key, value))
+          .join('');
+  }
+
+  createOptionElement(optionKey, optionText) {
+      return `
+      <div class="flex items-center border-[1px] border-green-700 hover:bg-yellow-200 cursor-pointer py-2 px-3 rounded-full gap-2">
+        <i class="fa-solid fa-key text-purple-500"></i>
+        <div class="${optionKey}">${optionText}</div>
+      </div>
+      `;
+  }
+}
